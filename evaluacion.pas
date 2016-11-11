e44a30f30019aec1eb96b7e1b48834e105e12677
Cristian Camilo Giraldo <cristian_camilo1505@hotmail.com>
2016-11-11 18:57:49 -0500

    capturado el error en el eval.pas
    sisasssssssss

--- evaluacion.pas
+++ evaluacion.pas
@@ -1,11 +1,12 @@
-PROGRAM EJER01;
+PROGRAM EJER01; 
+USES normal;
 	VAR a,b,c:INTEGER;
 BEGIN
 {Empezamos con lo basico, un programa qye escribe la suma de 2 numeros en pantalla}
 	a:=4;
 	b:=3;
 	{Se asigna un valor cualquiera a las variables "a" y "b"}
-	2c:=a+b;
+	c:=a+b;
 
 	WRITE(c); {Muestra Valor en pantalla}
 END
